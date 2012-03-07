87b1ef0875ba754ca4790be9490537288ad2642f
Misael Lopez Cruz <misael.lopez@ti.com>
2012-03-07 14:56:27 -0600

    tools: abe_dump: Use getopt for option parsing
    Replace existing option parsing method by getopt.
    
    Signed-off-by: Misael Lopez Cruz <misael.lopez@ti.com>

--- sound/soc/ti/aess/firmware/audio-test/tools/abe/abe_dump/abe_dump.c
+++ sound/soc/ti/aess/firmware/audio-test/tools/abe/abe_dump/abe_dump.c
@@ -21,11 +21,36 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <getopt.h>
 
-static void help(char *app_name)
+#define DMEM_ID		0x01
+#define CMEM_ID		0x02
+#define SMEM_ID		0x04
+#define PMEM_ID		0x08
+#define AESS_ID		0x10
+#define ALL_ID		0x1F
+
+#define DMEM_ADDR	0x49080000
+#define DMEM_SIZE	(64 * 1024)
+#define CMEM_ADDR	0x490A0000
+#define CMEM_SIZE	(8 * 1024)
+#define SMEM_ADDR	0x490C0000
+#define SMEM_SIZE	(24 * 1024)
+#define PMEM_ADDR	0x490E0000
+#define PMEM_SIZE	(8 * 1024)
+#define AESS_ADDR	0x490F1000
+#define AESS_SIZE	(1 * 1024)
+
+static void help(void)
 {
-	printf("%s: [DMEM] [CMEM] [SMEM] [PMEM] [AESS]\n", app_name);
-	printf("Alias: all, safe (no PMEM)\n");
+	printf("Usage: abe_dump [OPTION]\n");
+	printf("-d --dmem   Dump DMEM\n");
+	printf("-c --cmem   Dump CMEM\n");
+	printf("-s --smem   Dump SMEM\n");
+	printf("-p --pmem   Dump PMEM\n");
+	printf("-a --aess   Dump AESS registers\n");
+	printf("-h --help   Help\n");
+	printf("No option dumps all ABE mems\n");
 }
 
 static int dump_memory(char *filename, unsigned int baseaddr, unsigned int size)
@@ -73,44 +98,94 @@ err1:
 	return ret;
 }
 
-int main(int argc, char **argv)
+static const struct option long_options[] = {
+	{"dmem", no_argument, 0, 'd'},
+	{"cmem", no_argument, 0, 'c'},
+	{"smem", no_argument, 0, 's'},
+	{"pmem", no_argument, 0, 'p'},
+	{"aess", no_argument, 0, 'a'},
+	{"help", no_argument, 0, 'h'},
+	{0, 0, 0, 0},
+};
+
+static const char short_options[] = "dcspah";
+
+int main(int argc, char *argv[])
 {
-	int dmem = 0, cmem = 0, smem = 0, pmem = 0, aess = 0, i;
+	int dump = 0;
+	signed char c;
+	int ret;
 
-	if (argc < 2) {
-		printf("No memory specified\n");
-		help(argv[0]);
+	/* no args, dump all memories */
+	if (argc == 1)
+		dump = ALL_ID;
+
+	while ((c = getopt_long(argc, argv, short_options, long_options, 0)) != -1) {
+		switch (c) {
+		case 'd':
+			dump |= DMEM_ID;
+			break;
+		case 'c':
+			dump |= CMEM_ID;
+			break;
+		case 's':
+			dump |= SMEM_ID;
+			break;
+		case 'p':
+			dump |= PMEM_ID;
+			break;
+		case 'a':
+			dump |= AESS_ID;
+			break;
+		case 'h':
+			help();
+			return 0;
+		default:
+			help();
+			return 1;
+		};
 	}
 
-	for (i = 1; i < argc; i++) {
-		if (strstr("DMEM", argv[i]))
-			dmem = 1;
-		else if (strstr("CMEM", argv[i]))
-			cmem = 1;
-		else if (strstr("SMEM", argv[i]))
-			smem = 1;
-		else if (strstr("PMEM", argv[i]))
-			pmem = 1;
-		else if (strstr("AESS", argv[i]))
-			aess = 1;
-		else if (strstr("all", argv[i]))
-			dmem = cmem = smem = pmem = aess = 1;
-		else if (strstr("safe", argv[i]))
-			dmem = cmem = smem = aess = 1;
-		else
-			printf("invalid argument '%s', skipping\n", argv[i]);
+	if (dump & DMEM_ID) {
+		ret = dump_memory("dmem.txt", DMEM_ADDR, DMEM_SIZE);
+		if (ret) {
+			printf("Failed to dump DMEM\n");
+			return ret;
+		}
 	}
 
-	if (dmem)
-		dump_memory("dmem.txt", 0x49080000, 64 * 1024);
-	if (cmem)
-		dump_memory("cmem.txt", 0x490A0000, 8 * 1024);
-	if (smem)
-		dump_memory("smem.txt", 0x490C0000, 24 * 1024);
-	if (pmem)
-		dump_memory("pmem.txt", 0x490E0000, 8 * 1024);
-	if (aess)
-		dump_memory("aess.txt", 0x490F1000, 1 * 1024);
+	if (dump & CMEM_ID) {
+		ret = dump_memory("cmem.txt", CMEM_ADDR, CMEM_SIZE);
+		if (ret) {
+			printf("Failed to dump CMEM\n");
+			return ret;
+		}
+	}
+
+
+	if (dump & SMEM_ID) {
+		ret = dump_memory("smem.txt", SMEM_ADDR, SMEM_SIZE);
+		if (ret) {
+			printf("Failed to dump SMEM\n");
+			return ret;
+		}
+	}
+
+	if (dump & PMEM_ID) {
+		ret = dump_memory("pmem.txt", PMEM_ADDR, PMEM_SIZE);
+		if (ret) {
+			printf("Failed to dump PMEM\n");
+			return ret;
+		}
+	}
+
+	if (dump & AESS_ID) {
+		ret = dump_memory("aess.txt", AESS_ADDR, AESS_SIZE);
+		if (ret) {
+			printf("Failed to dump AESS registers\n");
+			return ret;
+		}
+	}
 
 	return 0;
 }

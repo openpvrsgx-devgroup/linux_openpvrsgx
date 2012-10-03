7dcbe7edb56026592c8f8dced92bfb49ff705a30
Patrick Titiano <p-titiano@ti.com>
2012-10-02 23:28:47 -0500

    SGX-KM: Add on3demand governor to sgxfreq
    Load based SGX DVFS governor.
    
    Change-Id: I4bb78d69bb154a5486a5d826ed7118498d68359f
    Signed-off-by: Hemant Hariyani <hemanthariyani@ti.com>

--- drivers/gpu/drm/pvrsgx/1.17.4948957/eurasia_km/services4/system/omap/sgxfreq.c
+++ drivers/gpu/drm/pvrsgx/1.17.4948957/eurasia_km/services4/system/omap/sgxfreq.c
@@ -24,11 +24,15 @@ int onoff_init(void);
 int onoff_deinit(void);
 int activeidle_init(void);
 int activeidle_deinit(void);
+int on3demand_init(void);
+int on3demand_deinit(void);
+
 
 typedef int sgxfreq_gov_init_t(void);
 sgxfreq_gov_init_t *sgxfreq_gov_init[] = {
 	onoff_init,
 	activeidle_init,
+	on3demand_init,
 	NULL,
 };
 
@@ -36,6 +40,7 @@ typedef int sgxfreq_gov_deinit_t(void);
 sgxfreq_gov_deinit_t *sgxfreq_gov_deinit[] = {
 	onoff_deinit,
 	activeidle_deinit,
+	on3demand_deinit,
 	NULL,
 };
 

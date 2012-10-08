29786d8085f6a7aa5e83107b9456df5f231bd797
Patrick Titiano <p-titiano@ti.com>
2012-10-08 14:35:20 -0700

    SGX-KM: Add 'userspace' governor to sgxfreq
    Add a new SGXFreq governor called "userspace".
    It is providing a direct control on the GPU clock speed from userspace, that
    can be used by various profiling tools for instance.
    Note it is relaxing whatever GPU frequency request set by user space
    when clock is off or sgx is idle.
    
    Change-Id: I44dcc5a948ae78c977b53654d4143ee735082c73
    Signed-off-by: myke smith <myke@ti.com>
    Signed-off-by: Hemant Hariyani <hemanthariyani@ti.com>
    Reviewed-on: https://gerrit.ext.ti.com/gerrit/omap/10284

--- drivers/gpu/drm/pvrsgx/1.14.3699939/eurasia_km/services4/system/omap/sysutils.c
+++ drivers/gpu/drm/pvrsgx/1.14.3699939/eurasia_km/services4/system/omap/sysutils.c
@@ -49,6 +49,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "sgxfreq_onoff.c"
 #include "sgxfreq_activeidle.c"
 #include "sgxfreq_on3demand.c"
+#include "sgxfreq_userspace.c"
 #if defined(CONFIG_THERMAL_FRAMEWORK)
 #include "sgxfreq_cool.c"
 #endif

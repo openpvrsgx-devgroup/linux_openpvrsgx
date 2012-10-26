51d6a6456d7605cc9a0b0266722075d149538279
Jeff McGee <j-mcgee@ti.com>
2012-10-26 10:36:47 -0500

    SGX-KM: sgxfreq userspace gov uses single frequency
    The userspace governor will set and maintain the userspace
    requested frequency no matter the state of SGX. Consequently
    the governor needs no event notifications. The governor will
    always default to the currently set frequency when started
    until userspace specifies a different one.
    
    Change-Id: I84a6e7d80fedd582f24cea989179605342141ddf
    Signed-off-by: Jeff McGee <j-mcgee@ti.com>
    Reviewed-on: https://gerrit.ext.ti.com/gerrit/omap/11011
    Reviewed-by: Hemant Hariyani <hemanthariyani@ti.com>
    Tested-by: Hemant Hariyani <hemanthariyani@ti.com>

--- drivers/gpu/drm/pvrsgx/1.14.3699939/eurasia_km/services4/system/omap/sgxfreq_userspace.c
+++ drivers/gpu/drm/pvrsgx/1.14.3699939/eurasia_km/services4/system/omap/sgxfreq_userspace.c
@@ -20,26 +20,17 @@
 
 static int userspace_start(struct sgxfreq_sgx_data *data);
 static void userspace_stop(void);
-static void userspace_sgx_clk_on(void);
-static void userspace_sgx_clk_off(void);
-static void userspace_sgx_active(void);
-static void userspace_sgx_idle(void);
 
 
 static struct sgxfreq_governor userspace_gov = {
 	.name =	"userspace",
 	.gov_start = userspace_start,
 	.gov_stop = userspace_stop,
-	.sgx_clk_on = userspace_sgx_clk_on,
-	.sgx_clk_off = userspace_sgx_clk_off,
-	.sgx_active = userspace_sgx_active,
-	.sgx_idle = userspace_sgx_idle,
 };
 
 
 static struct userspace_data {
-	unsigned long freq_user; /* in KHz */
-	struct mutex mutex;
+	unsigned long freq_user; /* in Hz */
 } usd;
 
 
@@ -65,14 +56,10 @@ static ssize_t store_frequency_set(struct device *dev,
 	if (ret != 1)
 		return -EINVAL;
 
-	mutex_lock(&odd.mutex);
-
 	if (freq > sgxfreq_get_freq_max())
 		freq = sgxfreq_get_freq_max();
 	usd.freq_user = sgxfreq_set_freq_request(freq);
 	trace_printk("USERSPACE: new freq=%luHz.\n", usd.freq_user);
-
-	mutex_unlock(&odd.mutex);
 
 	return count;
 }
@@ -99,8 +86,6 @@ static struct attribute_group userspace_attr_group = {
 int userspace_init(void)
 {
 	int ret;
-
-	mutex_init(&odd.mutex);
 
 	ret = sgxfreq_register_governor(&userspace_gov);
 	if (ret)
@@ -133,48 +118,7 @@ static int userspace_start(struct sgxfreq_sgx_data *data)
 
 static void userspace_stop(void)
 {
-	usd.freq_user = sgxfreq_set_freq_request(sgxfreq_get_freq_min());
 	sysfs_remove_group(sgxfreq_kobj, &userspace_attr_group);
 
 	trace_printk("USERSPACE: stopped.\n");
-}
-
-
-static void userspace_sgx_clk_on(void)
-{
-	mutex_lock(&ood.mutex);
-
-	sgxfreq_set_freq_request(usd.freq_user);
-
-	mutex_unlock(&ood.mutex);
-}
-
-
-static void userspace_sgx_clk_off(void)
-{
-	mutex_lock(&ood.mutex);
-
-	sgxfreq_set_freq_request(sgxfreq_get_freq_min());
-
-	mutex_unlock(&ood.mutex);
-}
-
-
-static void userspace_sgx_active(void)
-{
-	mutex_lock(&aid.mutex);
-
-	sgxfreq_set_freq_request(usd.freq_user);
-
-	mutex_unlock(&aid.mutex);
-}
-
-
-static void userspace_sgx_idle(void)
-{
-	mutex_lock(&aid.mutex);
-
-	sgxfreq_set_freq_request(sgxfreq_get_freq_min());
-
-	mutex_unlock(&aid.mutex);
 }

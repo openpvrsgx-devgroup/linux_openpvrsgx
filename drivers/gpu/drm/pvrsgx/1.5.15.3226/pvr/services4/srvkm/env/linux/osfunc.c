4ebd4aff70b8cc3bb2e432001e3157ede09427da
Thomas-Karl Pietrowski <thopiekar@googlemail.com>
2014-01-27 23:15:09 +0100

    changes to make build work on 3.11.x (Ubuntu Saucy) modifications on code taken from the yocto-project - http://git.yoctoproject.org/cgit.cgi/linux-yocto-3.10/?h=emgd-1.18

--- drivers/gpu/drm/pvrsgx/1.5.15.3226/pvr/services4/srvkm/env/linux/osfunc.c
+++ drivers/gpu/drm/pvrsgx/1.5.15.3226/pvr/services4/srvkm/env/linux/osfunc.c
@@ -2382,7 +2382,7 @@ PVRSRV_ERROR OSAcquirePhysPageAddr(IMG_VOID* pvCPUVAddr,
     }
 
 
-    if ((psVMArea->vm_flags & (VM_IO | VM_RESERVED)) != (VM_IO | VM_RESERVED))
+    if ((psVMArea->vm_flags & (VM_IO | VM_DONTEXPAND | VM_DONTDUMP)) != (VM_IO | VM_DONTEXPAND | VM_DONTDUMP))
     {
         PVR_DPF((PVR_DBG_ERROR,
             "OSAcquirePhysPageAddr: Memory region does not represent memory mapped I/O (VMA flags: 0x%lx)", psVMArea->vm_flags));

861c0b78359671e0aad1f5a67004fc622bde4cdb
Thomas-Karl Pietrowski <thopiekar@googlemail.com>
2014-07-23 22:26:08 +0200

    reverting some changes affecting older kernel versions

--- drivers/gpu/drm/pvrsgx/1.5.15.3226/pvr/services4/srvkm/env/linux/osfunc.c
+++ drivers/gpu/drm/pvrsgx/1.5.15.3226/pvr/services4/srvkm/env/linux/osfunc.c
@@ -2382,7 +2382,11 @@ PVRSRV_ERROR OSAcquirePhysPageAddr(IMG_VOID* pvCPUVAddr,
     }
 
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,3,0)
     if ((psVMArea->vm_flags & (VM_IO | VM_DONTEXPAND | VM_DONTDUMP)) != (VM_IO | VM_DONTEXPAND | VM_DONTDUMP))
+#else
+    if ((psVMArea->vm_flags & (VM_IO | VM_RESERVED)) != (VM_IO | VM_RESERVED))
+#endif
     {
         PVR_DPF((PVR_DBG_ERROR,
             "OSAcquirePhysPageAddr: Memory region does not represent memory mapped I/O (VMA flags: 0x%lx)", psVMArea->vm_flags));

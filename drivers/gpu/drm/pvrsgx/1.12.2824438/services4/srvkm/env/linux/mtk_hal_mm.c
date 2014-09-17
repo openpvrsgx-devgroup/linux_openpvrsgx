1a65d8bcf5860e3cb947f9f0201dd9481a6a4685
bq jenkins <anddevops@bq.com>
2014-09-17 15:19:12 +0200

    Version 2.0.0-20140910-1000 released

---
+++ drivers/gpu/drm/pvrsgx/1.12.2824438/services4/srvkm/env/linux/mtk_hal_mm.c
@@ -0,0 +1,15 @@
+#include <linux/sched.h>
+//#include "img_types.h"
+//#include <linux/string.h>	
+//#include "mtk_debug.h"
+//#include "servicesext.h"
+//#include "mutex.h"
+//#include "services.h"
+//#include "osfunc.h"
+
+#ifdef MTK_HAL_MM_STATISTIC
+
+atomic_t g_MtkSysRAMUseInByte_atomic = ATOMIC_INIT(0);
+
+#endif
+

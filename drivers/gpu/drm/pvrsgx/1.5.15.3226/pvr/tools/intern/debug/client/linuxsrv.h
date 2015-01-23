e98b64019f2b87a543f3d32312195fd04df014be
Thomas-Karl Pietrowski <thopiekar@googlemail.com>
2015-01-23 11:35:38 +0100

    Adding many lines which were removed after 3.10 fix + code style - module builds now back to 3.8 (tested)

--- drivers/gpu/drm/pvrsgx/1.5.15.3226/pvr/tools/intern/debug/client/linuxsrv.h
+++ drivers/gpu/drm/pvrsgx/1.5.15.3226/pvr/tools/intern/debug/client/linuxsrv.h
@@ -53,12 +53,12 @@ typedef struct tagIOCTL_PACKAGE
 	IMG_UINT32  ui32OutBufferSize;    // size of output data buffer
 } IOCTL_PACKAGE;
 
-IMG_UINT32 DeviceIoControl(IMG_UINT32 hDevice,		
-						IMG_UINT32 ui32ControlCode, 
-						IMG_VOID *pInBuffer,		
+IMG_UINT32 DeviceIoControl(IMG_UINT32 hDevice,
+						IMG_UINT32 ui32ControlCode,
+						IMG_VOID *pInBuffer,
 						IMG_UINT32 ui32InBufferSize,
-						IMG_VOID *pOutBuffer,		
-						IMG_UINT32 ui32OutBufferSize,  
-						IMG_UINT32 *pui32BytesReturned); 
+						IMG_VOID *pOutBuffer,
+						IMG_UINT32 ui32OutBufferSize,
+						IMG_UINT32 *pui32BytesReturned);
 
 #endif /* _LINUXSRV_H__*/

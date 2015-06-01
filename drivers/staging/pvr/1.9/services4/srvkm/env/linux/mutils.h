bb9b4df3c7f771b5e961ea2326e26098ff5b8db0
H. Nikolaus Schaller <hns@goldelico.com>
2015-06-01 16:53:17 +0200

    gpu: pvr: omap3/4: update source to Graphics_SDK_5_01_01_02
    http://software-dl.ti.com/dsps/dsps_public_sw/sdo_sb/targetcontent/gfxsdk/latest/index_FDS.html
    
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/staging/pvr/1.9/services4/srvkm/env/linux/mutils.h
+++ drivers/staging/pvr/1.9/services4/srvkm/env/linux/mutils.h
@@ -79,7 +79,11 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 	#define	IOREMAP(pa, bytes)	ioremap_cache(pa, bytes)
 #else	
 	#if defined(__arm__) && (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,0))
-		#define	IOREMAP(pa, bytes)	ioremap_cached(pa, bytes)
+		#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,13,0))
+			#define	IOREMAP(pa, bytes)	ioremap_cache(pa, bytes)
+		#else
+			#define	IOREMAP(pa, bytes)	ioremap_cached(pa, bytes)
+		#endif
 	#else
 		#define IOREMAP(pa, bytes)	ioremap(pa, bytes)
 	#endif

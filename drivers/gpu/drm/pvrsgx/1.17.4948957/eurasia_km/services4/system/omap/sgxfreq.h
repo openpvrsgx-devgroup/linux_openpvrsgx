3aa171d8ad26be588facb45d666dc88bb567feef
Hemant Hariyani <hemanthariyani@ti.com>
2015-10-07 19:04:05 -0500

    SGX-KM: Add sgxfreq subsystem for DVFS control
    Change-Id: I74c9ed39b785a9f39995643575aca6100361d148
    Signed-off-by: Jeff McGee <j-mcgee@ti.com>
    
    Conflicts:
    
    	eurasia_km/services4/system/omap/syslocal.h
    	eurasia_km/services4/system/omap/sysutils_linux.c

---
+++ drivers/gpu/drm/pvrsgx/1.17.4948957/eurasia_km/services4/system/omap/sgxfreq.h
@@ -0,0 +1,45 @@
+#ifndef SGXFREQ_H
+#define SGXFREQ_H
+
+#include <linux/device.h>
+
+#define SGXFREQ_NAME_LEN 16
+
+struct sgxfreq_governor {
+	char name[SGXFREQ_NAME_LEN];
+	int (*gov_start) (bool sgx_clk_on);
+	void (*gov_stop) (void);
+	void (*sgx_clk_on) (void);
+	void (*sgx_clk_off) (void);
+	struct list_head governor_list;
+};
+
+/* sgxfreq_init must be called before any other api */
+int sgxfreq_init(struct device *dev);
+int sgxfreq_deinit(void);
+
+int sgxfreq_register_governor(struct sgxfreq_governor *governor);
+void sgxfreq_unregister_governor(struct sgxfreq_governor *governor);
+
+int sgxfreq_set_governor(const char *name);
+
+int sgxfreq_get_freq_list(unsigned long **pfreq_list);
+
+unsigned long sgxfreq_get_freq_min(void);
+unsigned long sgxfreq_get_freq_max(void);
+
+unsigned long sgxfreq_get_freq_floor(unsigned long freq);
+unsigned long sgxfreq_get_freq_ceil(unsigned long freq);
+
+unsigned long sgxfreq_get_freq(void);
+unsigned long sgxfreq_get_freq_request(void);
+unsigned long sgxfreq_get_freq_limit(void);
+
+unsigned long sgxfreq_set_freq_request(unsigned long freq_request);
+unsigned long sgxfreq_set_freq_limit(unsigned long freq_limit);
+
+/* External notifications to sgxfreq */
+void sgxfreq_notif_sgx_clk_on(void);
+void sgxfreq_notif_sgx_clk_off(void);
+
+#endif

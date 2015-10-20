4ecb6f4cb4f67d37e8fdf5959e74c94a96847bd3
H. Nikolaus Schaller <hns@goldelico.com>
2015-10-20 12:14:38 +0200

    drivers:input:ads7846(+tsc2046): fix spi module table
    Fix module table so that the driver is loaded if compiled
    as module and requested by DT.
    
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/input/touchscreen/ads7846.c
+++ drivers/input/touchscreen/ads7846.c
@@ -1413,6 +1413,16 @@ static void ads7846_remove(struct spi_device *spi)
 	ads7846_stop(ts);
 }
 
+static const struct spi_device_id ads7846_idtable[] = {
+	{ "tsc2046", 0 },
+	{ "ads7843", 0 },
+	{ "ads7845", 0 },
+	{ "ads7846", 0 },
+	{ "ads7873", 0 },
+	{ }
+};
+MODULE_DEVICE_TABLE(spi, ads7846_idtable);
+
 static struct spi_driver ads7846_driver = {
 	.driver = {
 		.name		= "ads7846",
@@ -1428,4 +1438,3 @@ module_spi_driver(ads7846_driver);
 
 MODULE_DESCRIPTION("ADS7846 TouchScreen Driver");
 MODULE_LICENSE("GPL");
-MODULE_ALIAS("spi:ads7846");

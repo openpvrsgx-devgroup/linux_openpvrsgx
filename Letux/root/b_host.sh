a55f4b153898b6c6417daed8af3120f3ce6d4760
Andreas Kemnade <andreas@kemnade.info>
2015-12-11 17:10:06 +0100

    Letux: update paths in b_host.sh
    update aned cleanup path definitions in b_host.sh
    
    Signed-off-by: Andreas Kemnade <andreas@kemnade.info>

--- Letux/root/b_host.sh
+++ Letux/root/b_host.sh
@@ -2,18 +2,18 @@
 # if not in software-controlled charging mode, Vbus (and only that)
 # has to be applied before running the script
 #MUSB_MODE=/sys/bus/platform/devices/musb-hdrc/mode
-#MUSB_MODE=/sys/bus/platform/devices/musb-hdrc.0/mode
-MUSB_MODE=/sys/bus/platform/devices/musb-hdrc.1.auto/mode
-
+MUSB_MODE=$(echo /sys/bus/platform/devices/musb-hdrc*/mode)
+##MUSB_MODE=/sys/bus/platform/devices/musb-hdrc.1.auto/mode
+USB_PHY=$(echo /sys/bus/platform/devices/*twl4030-usb)
 MUSB_M=`cat $MUSB_MODE`
 echo "$MUSB_M"
 if [ "$MUSB_M" != b_idle ] ; then
 	echo Invalid mode at start
-        exit
+	exit
 fi
 # trigger a transition from b_idle to b_peripheral by simulating
 # a connection to a host (enable pull-down resistors)
-echo on > /sys/bus/platform/devices/twl4030_usb/fake_host
+echo on > $USB_PHY/fake_host
 sleep 0.2
 cat $MUSB_MODE
 # trigger a transition from b_peripheral to b_hnp_enable
@@ -21,12 +21,12 @@ echo host >$MUSB_MODE
 cat $MUSB_MODE
 sleep 0.2
 # simulate a disconnection from host
-echo off > /sys/bus/platform/devices/twl4030_usb/fake_host
+echo off > $USB_PHY/fake_host
 sleep 0.2
 cat $MUSB_MODE
 # be a host by ourselfs (if we wouldn't mess with this bit,
 # it would be already set here)
-echo on > /sys/bus/platform/devices/twl4030_usb/fake_host
+echo on > $USB_PHY/fake_host
 MUSB_M=`cat $MUSB_MODE`
 echo "$MUSB_M"
 

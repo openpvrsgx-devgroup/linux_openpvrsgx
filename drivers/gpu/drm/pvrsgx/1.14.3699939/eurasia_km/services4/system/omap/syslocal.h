dc5d60169c99e110215654958a9de79322556c0a
Eric Ruei <e-ruei1@ti.com>
2016-02-04 19:12:40 -0500

    KM: Enhance OMAP-based platform code to support AM3/4/5 devices
    - Add Configuration variable AM_VERSION to distinguish device AM3/4/5
    - Chnage both SYS_OMAP_GP11TIMER_XXX and SYS_OMAP_GP7TIMER_XXX to
      SYS_OMAP_GPTIMER_XXX
    
    Change-Id: Icc1c2b97b2317345aec851287b261edf4d7a1aa5
    Signed-off-by: Eric Ruei <e-ruei1@ti.com>

--- drivers/gpu/drm/pvrsgx/1.14.3699939/eurasia_km/services4/system/omap/syslocal.h
+++ drivers/gpu/drm/pvrsgx/1.14.3699939/eurasia_km/services4/system/omap/syslocal.h
@@ -88,13 +88,17 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <plat/gpu.h>
 #endif
 #if !defined(PVR_NO_OMAP_TIMER)
+#if (AM_VERSION == 5)
 #define	PVR_OMAP_USE_DM_TIMER_API
 #include <plat/dmtimer.h>
 #endif
 #endif
+#endif
 
 #if !defined(PVR_NO_OMAP_TIMER)
+#if (AM_VERSION == 5)
 #define PVR_OMAP_TIMER_BASE_IN_SYS_SPEC_DATA
+#endif
 #endif
 #endif /* defined(__linux__) */
 
@@ -107,7 +111,9 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #if defined(SGX_OCP_REGS_ENABLED)
 /* FIXME: Temporary workaround for OMAP4470 and active power off in 4430 */
 #if !defined(SGX544) && defined(SUPPORT_ACTIVE_POWER_MANAGEMENT)
+#if (AM_VERSION == 5)
 #define SGX_OCP_NO_INT_BYPASS
+#endif
 #endif
 #endif
 #endif

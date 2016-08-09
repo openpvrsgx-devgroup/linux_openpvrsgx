e7d44bb31a8fe98bc0f722e7b9abb94d135eddb4
Hans de Goede <hdegoede@redhat.com>
2016-08-09 23:27:00 +0200

    Fix build against kernel 4.8
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- drivers/staging/rtl8189es/os_dep/linux/ioctl_cfg80211.c
+++ drivers/staging/rtl8189es/os_dep/linux/ioctl_cfg80211.c
@@ -2037,7 +2037,15 @@ void rtw_cfg80211_indicate_scan_done(_adapter *adapter, bool aborted)
 		}
 		else
 		{
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 8, 0))
+			struct cfg80211_scan_info info = {
+				.aborted = aborted,
+			};
+
+			cfg80211_scan_done(pwdev_priv->scan_request, &info);
+#else
 			cfg80211_scan_done(pwdev_priv->scan_request, aborted);
+#endif
 		}
 
 		pwdev_priv->scan_request = NULL;

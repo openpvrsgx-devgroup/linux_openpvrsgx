44e3eb10af2d6e41247f3e2ff62960643fbae2b0
Anand Balagopalakrishnan <anandb@ti.com>
2016-10-13 10:50:13 +0530

    km: build: remove dependency on user-mode builds
    Change-Id: Ifb8776e1bde6b0c3328097d4568ca8f8a0dcdf04
    Signed-off-by: Anand Balagopalakrishnan <anandb@ti.com>

--- drivers/gpu/drm/pvrsgx/1.14.3699939/eurasia_km/eurasiacon/build/linux2/toplevel.mk
+++ drivers/gpu/drm/pvrsgx/1.14.3699939/eurasia_km/eurasiacon/build/linux2/toplevel.mk
@@ -1,41 +1,6 @@
 ########################################################################### ###
 #@Copyright     Copyright (c) Imagination Technologies Ltd. All Rights Reserved
-#@License       Dual MIT/GPLv2
-# 
-# The contents of this file are subject to the MIT license as set out below.
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-# 
-# Alternatively, the contents of this file may be used under the terms of
-# the GNU General Public License Version 2 ("GPL") in which case the provisions
-# of GPL are applicable instead of those above.
-# 
-# If you wish to allow use of your version of this file only under the terms of
-# GPL, and not to allow others to use your version of this file under the terms
-# of the MIT license, indicate your decision by deleting the provisions above
-# and replace them with the notice and other provisions required by GPL as set
-# out in the file called "GPL-COPYING" included in this distribution. If you do
-# not delete the provisions above, a recipient may use your version of this file
-# under the terms of either the MIT license or GPL.
-# 
-# This License is also included in this distribution in the file called
-# "MIT-COPYING".
-# 
-# EXCEPT AS OTHERWISE STATED IN A NEGOTIATED AGREEMENT: (A) THE SOFTWARE IS
-# PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
-# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-# PURPOSE AND NONINFRINGEMENT; AND (B) IN NO EVENT SHALL THE AUTHORS OR
-# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#@License       Strictly Confidential.
 ### ###########################################################################
 
 # Define the default goal. This masks a previous definition of the default
@@ -145,6 +110,7 @@ INTERNAL_INCLUDED_ALL_MAKEFILES :=
 
 ifneq ($(INTERNAL_CLOBBER_ONLY),true)
 # Please do not change the format of the following lines
+-include $(CONFIG_MK)
 -include $(CONFIG_KERNEL_MK)
 # These files may not exist in GPL km source packages
 -include $(MAKE_TOP)/xorgconf.mk
@@ -177,7 +143,7 @@ kbuild install:
 ifneq ($(INTERNAL_CLOBBER_ONLY),true)
 -include $(MAKE_TOP)/scripts.mk
 -include $(MAKE_TOP)/kbuild/kbuild.mk
-else
+endif
 # We won't depend on 'build' here so that people can build subsets of
 # components and still have the install script attempt to install the
 # subset.
@@ -197,7 +163,6 @@ install:
 		exit 1; \
 	fi
 	@cd $(TARGET_OUT) && ./install.sh
-endif
 
 # You can say 'make all_modules' to attempt to make everything, or 'make
 # components' to only make the things which are listed (in the per-build

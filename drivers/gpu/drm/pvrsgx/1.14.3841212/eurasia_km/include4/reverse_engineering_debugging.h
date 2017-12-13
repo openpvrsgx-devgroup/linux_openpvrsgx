261464fcc0b93051030ec124f13d31a6979f0758
Philipp Rossak <embed3d@gmail.com>
2017-12-13 21:51:10 +0100

    impelementation of own Debugging function
    The implemented debugging printouts are somehow broken.
    To receive Debugging infos, 4 debugging functions are implemented
    
    DEBUG_REG_R: Prints the adress of a read operation (only offset)
    DEBUG_REG_W: Prints the adress and data of a write operation
    RVDBGCOMMENT: Replacment of PDUMPCOMMENT
    DBG_LOG: Replacment of PVR_LOG

--- drivers/gpu/drm/pvrsgx/1.14.3841212/eurasia_km/include4/reverse_engineering_debugging.h
+++ drivers/gpu/drm/pvrsgx/1.14.3841212/eurasia_km/include4/reverse_engineering_debugging.h
@@ -1,5 +1,36 @@
 #ifdef CONFIG_PVR_DRIVER_DEBUG
-#define RV_DBG(x) printk(KERN_ALERT "##PVR_RV_DBG %s", x)
+
+#include "include/linux/string.h"
+#include "include/linux/kernel.h"
+void printdbgcomment(char *string, ...){
+
+	va_list aptr;
+	char buff[120];
+
+	va_start(aptr,string);
+	vsprintf(buff,string, aptr);
+	va_end(aptr);
+
+	printk(KERN_ALERT "#PVR_DBG: %s\n", buff);
+}
+
+void printwreg(char *dumpregname, u32 reg, u32 data){
+
+	printk(KERN_ALERT "#PVR_DBG: WRW: %s: Reg: 0x%08X  Data: 0x%08X\n", dumpregname, reg, data);
+}
+
+void printrreg(char *dumpregname, u32 offset){
+
+	printk(KERN_ALERT "#PVR_DBG: RDW: %s: Reg: 0x%08X  ", dumpregname, offset);
+}
+
+
+#define DEBUG_REG_R printrreg
+#define DEBUG_REG_W printwreg
+
+#define RVDBGCOMMENT printdbgcomment
+#define DBG_LOG printdbgcomment
+
 #else
 #define RV_DBG(x)
 #endif

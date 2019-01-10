226c23a6a888397a48493c300958a0447f02d042
H. Nikolaus Schaller <hns@goldelico.com>
2019-01-10 09:17:45 +0100

    gpu: pvr: osfunc,pvr_uaccess: fix v5.0 API change
    access_ok() changed from 3 arguments to 2
    
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/gpu/drm/pvrsgx/1.14.3699939/eurasia_km/services4/srvkm/env/linux/pvr_uaccess.h
+++ drivers/gpu/drm/pvrsgx/1.14.3699939/eurasia_km/services4/srvkm/env/linux/pvr_uaccess.h
@@ -49,12 +49,19 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #endif
 #endif
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,18))
+#include <linux/uaccess.h>
+#endif
 #include <asm/uaccess.h>
 
 static inline unsigned long pvr_copy_to_user(void __user *pvTo, const void *pvFrom, unsigned long ulBytes)
 {
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,33))
-    if (access_ok(VERIFY_WRITE, pvTo, ulBytes))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,0,0))
+     if (access_ok(pvTo, ulBytes))
+#else
+     if (access_ok(VERIFY_WRITE, pvTo, ulBytes))
+#endif
     {
 	return __copy_to_user(pvTo, pvFrom, ulBytes);
     }
@@ -71,7 +78,11 @@ static inline unsigned long pvr_copy_from_user(void *pvTo, const void __user *pv
      * The compile time correctness checking introduced for copy_from_user in
      * Linux 2.6.33 isn't fully comaptible with our usage of the function.
      */
-    if (access_ok(VERIFY_READ, pvFrom, ulBytes))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,0,0))
+     if (access_ok(pvFrom, ulBytes))
+#else
+     if (access_ok(VERIFY_READ, pvFrom, ulBytes))
+#endif
     {
 	return __copy_from_user(pvTo, pvFrom, ulBytes);
     }

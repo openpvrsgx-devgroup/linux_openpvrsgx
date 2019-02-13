9ffd652f7ce01ba50bf1bac1aa5ed9fe22874382
H. Nikolaus Schaller <hns@goldelico.com>
2019-02-13 10:53:46 +0100

    gpu: pvr: omap3/4: module,pvr_uaccess: fix some API changes not for 4.16 but 5.0
    I have no idea why there was the assumption that these access_ok() changes are needed for 4.16 and later.
    In fact access_ok() lost its first argument only in 5.0-rc1
    
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/gpu/drm/pvrsgx/1.10.2359475/services4/srvkm/env/linux/pvr_uaccess.h
+++ drivers/gpu/drm/pvrsgx/1.10.2359475/services4/srvkm/env/linux/pvr_uaccess.h
@@ -58,7 +58,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 static inline unsigned long pvr_copy_to_user(void __user *pvTo, const void *pvFrom, unsigned long ulBytes)
 {
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,33))
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,16,0))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,0,0))
     if (access_ok(pvTo, ulBytes))
 #else
     if (access_ok(VERIFY_WRITE, pvTo, ulBytes))
@@ -79,7 +79,7 @@ static inline unsigned long pvr_copy_from_user(void *pvTo, const void __user *pv
      * The compile time correctness checking introduced for copy_from_user in
      * Linux 2.6.33 isn't fully comaptible with our usage of the function.
      */
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,16,0))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,0,0))
    if (access_ok(pvFrom, ulBytes))
 #else
    if (access_ok(VERIFY_READ, pvFrom, ulBytes))

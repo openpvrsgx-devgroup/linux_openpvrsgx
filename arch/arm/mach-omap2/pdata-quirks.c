goldelico/letux-kernel

ARM: OMAP2+: pdata-quirks: add SGX support for OMAP3 and am33xx
We need to define the SGX module base address for each SoC.
We fix the name for the am33xx to follow the pattern "SoC-gpu".

Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

arch/arm/mach-omap2/pdata-quirks.c
@@ -49,7 +49,7 @@ struct pdata_init {
 static struct of_dev_auxdata omap_auxdata_lookup[];
 static struct twl4030_gpio_platform_data twl_gpio_auxdata;
 
-#if defined(CONFIG_SOC_AM33XX) || defined(CONFIG_SOC_AM43XX)
+#if defined(CONFIG_SOC_AM33XX) || defined(CONFIG_SOC_AM43XX) || defined(CONFIG_SGX530)
 static struct gfx_sgx_platform_data sgx_pdata = {
 	.reset_name = "gfx",
 	.assert_reset = omap_device_assert_hardreset,
@@ -562,6 +562,8 @@ static struct of_dev_auxdata omap_auxdata_lookup[] __initdata = {
 		       &omap3_iommu_pdata),
 	OF_DEV_AUXDATA("ti,omap3-hsmmc", 0x4809c000, "4809c000.mmc", &mmc_pdata[0]),
 	OF_DEV_AUXDATA("ti,omap3-hsmmc", 0x480b4000, "480b4000.mmc", &mmc_pdata[1]),
+	OF_DEV_AUXDATA("ti,omap3-gpu", 0x50000000, "50000000.sgx",
+		       &sgx_pdata),
 	OF_DEV_AUXDATA("nokia,n900-ir", 0, "n900-ir", &rx51_ir_data),
 	/* Only on am3517 */
 	OF_DEV_AUXDATA("ti,davinci_mdio", 0x5c030000, "davinci_mdio.0", NULL),
@@ -585,7 +587,7 @@ static struct of_dev_auxdata omap_auxdata_lookup[] __initdata = {
 	OF_DEV_AUXDATA("ti,omap-dmtimer-pwm", 0, NULL, &pwm_dmtimer_pdata),
 #endif
 #if defined(CONFIG_SOC_AM33XX) || defined(CONFIG_SOC_AM43XX)
-	OF_DEV_AUXDATA("ti,sgx", 0x56000000, "56000000.sgx",
+	OF_DEV_AUXDATA("ti,am33xx-gpu", 0x56000000, "56000000.sgx",
 		       &sgx_pdata),
 #endif
 #if defined(CONFIG_ARCH_OMAP4) || defined(CONFIG_SOC_OMAP5)

openpvrsgx-devgroup/linux_openpvrsgx

arch/arm/mach-omap2/pdata-quirks.c
@@ -46,7 +46,7 @@ struct pdata_init {
 static struct of_dev_auxdata omap_auxdata_lookup[];
 static struct twl4030_gpio_platform_data twl_gpio_auxdata;
 
-#if defined(CONFIG_SOC_AM33XX) || defined(CONFIG_SOC_AM43XX)
+#if defined(CONFIG_SOC_AM33XX) || defined(CONFIG_SOC_AM43XX) || defined(CONFIG_ARCH_OMAP3)
 static struct gfx_sgx_platform_data sgx_pdata = {
 	.reset_name = "gfx",
 	.assert_reset = omap_device_assert_hardreset,
@@ -632,6 +632,8 @@ static struct of_dev_auxdata omap_auxdata_lookup[] = {
 		       "480c9000.smartreflex", &omap_sr_pdata[OMAP_SR_MPU]),
 	OF_DEV_AUXDATA("ti,omap3-hsmmc", 0x4809c000, "4809c000.mmc", &mmc_pdata[0]),
 	OF_DEV_AUXDATA("ti,omap3-hsmmc", 0x480b4000, "480b4000.mmc", &mmc_pdata[1]),
+	OF_DEV_AUXDATA("ti,omap3-gpu", 0x50000000, "50000000.sgx",
+		       &sgx_pdata),
 	/* Only on am3517 */
 	OF_DEV_AUXDATA("ti,davinci_mdio", 0x5c030000, "davinci_mdio.0", NULL),
 	OF_DEV_AUXDATA("ti,am3517-emac", 0x5c000000, "davinci_emac.0",
@@ -651,7 +653,7 @@ static struct of_dev_auxdata omap_auxdata_lookup[] = {
 		       &wkup_m3_data),
 #endif
 #if defined(CONFIG_SOC_AM33XX) || defined(CONFIG_SOC_AM43XX)
-	OF_DEV_AUXDATA("ti,sgx", 0x56000000, "56000000.sgx",
+	OF_DEV_AUXDATA("ti,am33xx-gpu", 0x56000000, "56000000.sgx",
 		       &sgx_pdata),
 #endif
 #if defined(CONFIG_ARCH_OMAP4) || defined(CONFIG_SOC_OMAP5)
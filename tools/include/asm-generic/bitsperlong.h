848ed9da4ff532dc9eace1f61ff74c55157e392b
H. Nikolaus Schaller <hns@goldelico.com>
2019-10-02 10:15:55 +0200

    tools: asm-generic: introduce asm/bitsperlong.h
    apply the same "fixme" as for
    
    commit c31ae4bb4a9fa4606a74c0a4fb61b74f804e861e
    Author: Arnd Bergmann <arnd@arndb.de>
    Date:   Wed May 13 22:56:25 2009 +0000
    
        asm-generic: introduce asm/bitsperlong.h
    
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- tools/include/asm-generic/bitsperlong.h
+++ tools/include/asm-generic/bitsperlong.h
@@ -10,7 +10,11 @@
 #define BITS_PER_LONG __WORDSIZE
 #endif
 
-#if BITS_PER_LONG != __BITS_PER_LONG
+/*
+ * FIXME: The check currently breaks x86-64 build, so it's
+ * temporarily disabled. Please fix x86-64 and reenable
+ */
+#if 0 && BITS_PER_LONG != __BITS_PER_LONG
 #error Inconsistent word size. Check asm/bitsperlong.h
 #endif
 

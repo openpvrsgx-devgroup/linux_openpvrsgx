3fb80fc25418e69c521c938724f8c8e1499b37b6
H. Nikolaus Schaller <hns@goldelico.com>
2019-12-09 16:41:11 +0100

    gpu: pvr: 1.14.3699939 since v5.5 we have no drmP.h any more
    It was marked as obsolete and recommended to be no longer used.
    Therfore we include it only for KERNEL_VERSION < 5.5
    and in some cases directly include some drm specific files which
    were part of drmP.h
    
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/gpu/drm/pvrsgx/1.14.3699939/eurasia_km/services4/3rdparty/dc_nohw/dc_nohw_linux.c
+++ drivers/gpu/drm/pvrsgx/1.14.3699939/eurasia_km/services4/3rdparty/dc_nohw/dc_nohw_linux.c
@@ -79,7 +79,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <linux/pci.h>
 #include <linux/slab.h>
 
-#if defined(SUPPORT_DRI_DRM)
+#if defined(SUPPORT_DRI_DRM) && (LINUX_VERSION_CODE < KERNEL_VERSION(5,5,0))
 #include <drm/drmP.h>
 #endif
 

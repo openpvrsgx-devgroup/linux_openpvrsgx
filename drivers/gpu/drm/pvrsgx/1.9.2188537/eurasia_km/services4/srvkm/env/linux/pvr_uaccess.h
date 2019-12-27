14a666102b2d28a526d533c527eef00ccbc490b7
Jeroen Hofstee <jeroen@myspectrum.nl>
2019-12-27 16:57:00 +0100

    access_ok dropped the type argument
    Signed-off-by: Jeroen Hofstee <jeroen@myspectrum.nl>

--- drivers/gpu/drm/pvrsgx/1.9.2188537/eurasia_km/services4/srvkm/env/linux/pvr_uaccess.h
+++ drivers/gpu/drm/pvrsgx/1.9.2188537/eurasia_km/services4/srvkm/env/linux/pvr_uaccess.h
@@ -53,15 +53,12 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 static inline unsigned long pvr_copy_to_user(void __user *pvTo, const void *pvFrom, unsigned long ulBytes)
 {
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,33))
-    if (access_ok(VERIFY_WRITE, pvTo, ulBytes))
-    {
-	return __copy_to_user(pvTo, pvFrom, ulBytes);
-    }
-    return ulBytes;
-#else
-    return copy_to_user(pvTo, pvFrom, ulBytes);
-#endif
+	if (access_ok(pvTo, ulBytes))
+	{
+		return __copy_to_user(pvTo, pvFrom, ulBytes);
+	}
+	// REALLY?
+	return ulBytes;
 }
 
 static inline unsigned long pvr_copy_from_user(void *pvTo, const void __user *pvFrom, unsigned long ulBytes)
@@ -71,7 +68,7 @@ static inline unsigned long pvr_copy_from_user(void *pvTo, const void __user *pv
      * The compile time correctness checking introduced for copy_from_user in
      * Linux 2.6.33 isn't fully comaptible with our usage of the function.
      */
-    if (access_ok(VERIFY_READ, pvFrom, ulBytes))
+    if (access_ok(pvFrom, ulBytes))
     {
 	return __copy_from_user(pvTo, pvFrom, ulBytes);
     }

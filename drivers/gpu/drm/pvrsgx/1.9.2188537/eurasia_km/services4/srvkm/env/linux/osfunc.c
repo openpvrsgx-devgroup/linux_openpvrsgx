ce11673f7f03b8f055871c380a09a00b04b4e6e0
Jeroen Hofstee <jeroen@myspectrum.nl>
2019-12-27 16:57:39 +0100

    gpu: pvr: omap3/4: osfunc: get_user_pages() has changed parameters
    fix parameters and replace 1, 0 by FOLL_WRITE
    principle described at https://github.com/korena/bbb-workshop/blob/master/SGX530_GPU.md
    
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>
    Signed-off-by: Jeroen Hofstee <jeroen@myspectrum.nl>

--- drivers/gpu/drm/pvrsgx/1.9.2188537/eurasia_km/services4/srvkm/env/linux/osfunc.c
+++ drivers/gpu/drm/pvrsgx/1.9.2188537/eurasia_km/services4/srvkm/env/linux/osfunc.c
@@ -3604,7 +3604,7 @@ PVRSRV_ERROR OSAcquirePhysPageAddr(IMG_VOID *pvCPUVAddr,
     bMMapSemHeld = IMG_TRUE;
 
     /* Get page list */
-    psInfo->iNumPagesMapped = get_user_pages(current, current->mm, ulStartAddr, psInfo->iNumPages, 1, 0, psInfo->ppsPages, NULL);
+    psInfo->iNumPagesMapped = get_user_pages(ulStartAddr, psInfo->iNumPages, FOLL_WRITE, psInfo->ppsPages, NULL);
 
     if (psInfo->iNumPagesMapped >= 0)
     {

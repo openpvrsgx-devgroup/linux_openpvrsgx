25cdb1bce3bebb7985a832dc5ae2416cd4024806
Jeroen Hofstee <jeroen@myspectrum.nl>
2019-12-27 17:01:41 +0100

    remove build output
    Don't store generated files and build output in git.
    
    Signed-off-by: Jeroen Hofstee <jeroen@myspectrum.nl>

--- drivers/gpu/drm/pvrsgx/1.9.2188537/eurasia_km/services4/3rdparty/bufferclass_ti/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home1/prathap/delete/Graphics_SDK_4_08_00_01/GFX_Linux_KM/services4/3rdparty/bufferclass_ti/built-in.o :=  rm -f /home1/prathap/delete/Graphics_SDK_4_08_00_01/GFX_Linux_KM/services4/3rdparty/bufferclass_ti/built-in.o; arm-arago-linux-gnueabi-ar rcsD /home1/prathap/delete/Graphics_SDK_4_08_00_01/GFX_Linux_KM/services4/3rdparty/bufferclass_ti/built-in.o

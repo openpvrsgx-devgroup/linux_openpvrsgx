602079117ff31fb7acf22a604388ec2ddfca5c9e
H. Nikolaus Schaller <hns@goldelico.com>
2020-02-20 06:54:26 +0100

    drm: panel-pinephone-jd9365da: make it compile on v5.6
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/gpu/drm/panel/panel-pinephone-jd9365da.c
+++ drivers/gpu/drm/panel/panel-pinephone-jd9365da.c
@@ -407,13 +407,13 @@ static const struct drm_display_mode pinephone_default_mode = {
 	.height_mm	= 130,
 };
 
-static int pinephone_get_modes(struct drm_panel *panel)
+static int pinephone_get_modes(struct drm_panel *panel,
+				 struct drm_connector *connector)
 {
-	struct drm_connector *connector = panel->connector;
 	struct pinephone *ctx = panel_to_pinephone(panel);
 	struct drm_display_mode *mode;
 
-	mode = drm_mode_duplicate(panel->drm, &pinephone_default_mode);
+	mode = drm_mode_duplicate(connector->dev, &pinephone_default_mode);
 	if (!mode) {
 		DRM_DEV_ERROR(&ctx->dsi->dev, "failed to add mode %ux%ux@%u\n",
 			      pinephone_default_mode.hdisplay,
@@ -425,8 +425,8 @@ static int pinephone_get_modes(struct drm_panel *panel)
 	drm_mode_set_name(mode);
 
 	drm_mode_probed_add(connector, mode);
-	panel->connector->display_info.width_mm = mode->width_mm;
-	panel->connector->display_info.height_mm = mode->height_mm;
+	connector->display_info.width_mm = mode->width_mm;
+	connector->display_info.height_mm = mode->height_mm;
 
 	return 1;
 }

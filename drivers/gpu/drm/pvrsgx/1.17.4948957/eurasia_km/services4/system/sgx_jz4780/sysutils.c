56b872acf844d30d7de8ab3b2eb7e6ac786cc445
H. Nikolaus Schaller <hns@goldelico.com>
2020-03-28 13:18:30 +0100

    gpu: pvr: sgx_jz4780: sysutils.c: add SysSGXIdleEntered and SysSGXCommandPending helper functions introduced by TI for omap also to jz4780
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/gpu/drm/pvrsgx/1.17.4948957/eurasia_km/services4/system/sgx_jz4780/sysutils.c
+++ drivers/gpu/drm/pvrsgx/1.17.4948957/eurasia_km/services4/system/sgx_jz4780/sysutils.c
@@ -392,3 +392,20 @@ PVRSRV_ERROR SysDvfsDeinitialize(SYS_SPECIFIC_DATA *psSysSpecificData)
 #endif 
 	return PVRSRV_OK;
 }
+
+IMG_VOID SysSGXIdleEntered(IMG_VOID)
+{
+#if defined(SYS_OMAP_HAS_DVFS_FRAMEWORK)
+	sgxfreq_notif_sgx_idle();
+#endif
+}
+
+IMG_VOID SysSGXCommandPending(IMG_BOOL bSGXIdle)
+{
+#if defined(SYS_OMAP_HAS_DVFS_FRAMEWORK)
+	if (bSGXIdle)
+		sgxfreq_notif_sgx_active();
+#else
+	PVR_UNREFERENCED_PARAMETER(bSGXIdle);
+#endif
+}

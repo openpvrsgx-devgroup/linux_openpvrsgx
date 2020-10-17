ccf2431a960b42735663b81d2fc92afbb8e40704
H. Nikolaus Schaller <hns@goldelico.com>
2020-10-17 11:40:53 +0200

    gpu: pvr: 1.14.3699939: poulsbo: sysutils: define empty SysSGXIdleEntered() and SysSGXCommandPending()
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/gpu/drm/pvrsgx/1.14.3699939/eurasia_km/services4/system/poulsbo/sysutils.c
+++ drivers/gpu/drm/pvrsgx/1.14.3699939/eurasia_km/services4/system/poulsbo/sysutils.c
@@ -102,3 +102,13 @@ IMG_VOID SysPowerMaybeInit(SYS_DATA *psSysData)
 }
 
 #endif /* defined(PVR_LINUX_USING_WORKQUEUES) */
+
+IMG_VOID SysSGXIdleEntered(IMG_VOID)
+{
+	return;
+}
+
+IMG_VOID SysSGXCommandPending(IMG_BOOL bSGXIdle)
+{
+	return;
+}

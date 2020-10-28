189eebf27cfdf9ffd3b9573980cf2b0aa3b2ecc3
H. Nikolaus Schaller <hns@goldelico.com>
2020-10-28 11:51:42 +0100

    drm: panel-pinephone-jd9365da: make it compile on v5.10
    drm_panel_add() now returns void.
    
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/gpu/drm/panel/panel-pinephone-jd9365da.c
+++ drivers/gpu/drm/panel/panel-pinephone-jd9365da.c
@@ -443,7 +443,6 @@ static const struct drm_panel_funcs pinephone_funcs = {
 static int pinephone_dsi_probe(struct mipi_dsi_device *dsi)
 {
 	struct pinephone *ctx;
-	int ret;
 
 	ctx = devm_kzalloc(&dsi->dev, sizeof(*ctx), GFP_KERNEL);
 	if (!ctx)
@@ -483,10 +482,7 @@ static int pinephone_dsi_probe(struct mipi_dsi_device *dsi)
 	if (IS_ERR(ctx->backlight))
 		return PTR_ERR(ctx->backlight);
 
-	ret = drm_panel_add(&ctx->panel);
-	if (ret < 0) {
-		return ret;
-	}
+	drm_panel_add(&ctx->panel);
 	dsi->mode_flags = MIPI_DSI_MODE_VIDEO_SYNC_PULSE;
 	dsi->format = MIPI_DSI_FMT_RGB888;
 	dsi->lanes = 4;

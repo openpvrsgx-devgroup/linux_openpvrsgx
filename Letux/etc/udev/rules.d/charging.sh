ef57443d7beaa26897913f05c1072cccd46fbd73
H. Nikolaus Schaller <hns@goldelico.com>
2022-06-04 22:32:28 +0200

    Letux: rootfs: udev: charging: update to handle Pyra (experimental)
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- Letux/etc/udev/rules.d/charging.sh
+++ Letux/etc/udev/rules.d/charging.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
 # detect Openmoko wall charger and increase input current limit
 
-case $(cat /dev/usb_id 2>/dev/null) in
-	"floating" ) echo "500000" >/dev/usb_max_current;;
-	"102k" ) echo "851000" >/dev/usb_max_current;;
-	"GND" ) echo "851000" >/dev/usb_max_current;;
-esac
+case "$1" in
+"pyra" )
+	# if wwan is now on (and usb is charging) => limit system voltage
+	# check if USB is connected? cat /sys/class/extcon/extcon0/cable.0/state
+	# check if USB-host is connected? cat /sys/class/extcon/extcon0/cable.1/state
+	BQ24297=/sys/class/power_supply/bq24297
+	RFKILL_STATE=
+	USB_STATE=
 
-# remove # to enable
-# echo $(date) "$SUBSYSTEM $ACTION" $(cat /dev/usb_id 2>/dev/null) $(cat /dev/usb_max_current 2>/dev/null) >>/tmp/charger.log
+	if rfkill list wwan | fgrep "Soft blocked: no" -q
+	then # limit max. charging voltage
+		echo 4050000
+	else # unlimit voltage
+		echo 4200000
+	fi >$BQ24297/constant_charge_voltage_max
+
+	echo $(date) "$SUBSYSTEM $ACTION" $(cat $BQ24297/constant_charge_voltage_max 2>/dev/null) >>/tmp/charger.log
+
+	;;
+
+gta04 | * )
+	# FIXME: replace /dev/usb_id mechanism by access to /sys/class/power_supply/twl4030 or similar
+
+	case $(cat /dev/usb_id 2>/dev/null) in
+		"floating" ) echo "500000" >/dev/usb_max_current;;
+		"102k" ) echo "851000" >/dev/usb_max_current;;
+		"GND" ) echo "851000" >/dev/usb_max_current;;
+	esac
+
+	# remove # to enable
+	echo $(date) "$SUBSYSTEM $ACTION" $(cat /dev/usb_id 2>/dev/null) $(cat /dev/usb_max_current 2>/dev/null) >>/tmp/charger.log
+	;;
+
+esac

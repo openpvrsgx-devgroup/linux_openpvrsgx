e8a533109731515fa1bc5e82792c445ff8f58341
H. Nikolaus Schaller <hns@goldelico.com>
2022-08-27 13:15:10 +0200

    ASoC: si47xx, w2cbw003-bt: replace non_legacy_dai_naming = 1 with legacy_dai_naming = 0
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- sound/soc/codecs/si47xx.c
+++ sound/soc/codecs/si47xx.c
@@ -269,7 +269,6 @@ static struct snd_soc_component_driver soc_component_dev_si47xx = {
 	.idle_bias_on		= 1,
 	.use_pmdown_time	= 1,
 	.endianness		= 1,
-	.non_legacy_dai_naming	= 1,
 };
 
 static int si47xx_suspend(struct snd_soc_component *component)
--- sound/soc/codecs/w2cbw003-bt.c
+++ sound/soc/codecs/w2cbw003-bt.c
@@ -75,7 +75,6 @@ struct snd_soc_component_driver soc_component_dev_w2cbw003 = {
 	.idle_bias_on		= 1,
 	.use_pmdown_time	= 1,
 	.endianness		= 1,
-	.non_legacy_dai_naming	= 1,
 };
 
 static int w2cbw003_platform_probe(struct platform_device *pdev)

39bd42e9618320946ab4064056cac933adcf171e
Patrik Dahlström <risca@powerlamerz.org>
2022-11-04 23:28:57 +0100

    Use full executable path

--- Letux/root/pyra_vol_mon/main.c
+++ Letux/root/pyra_vol_mon/main.c
@@ -34,7 +34,7 @@ static int execute_callback(const char* prog, int value)
 	snprintf(value_string, sizeof(value_string), "%d", value);
 
 	if (0 == (my_pid = fork())) {
-		if (-1 == execlp(prog, basename(prog), value_string, 0)) {
+		if (-1 == execlp(prog, prog, value_string, 0)) {
 			perror("child process execve failed");
 			return -1;
 		}

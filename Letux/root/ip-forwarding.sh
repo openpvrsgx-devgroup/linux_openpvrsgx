f22dd480b5bf35981768d22dd0bfced058c110b6
H. Nikolaus Schaller <hns@goldelico.com>
2023-09-07 19:21:04 +0200

    Letux: rootfs: ip-forwarding.sh: rework Darwin version
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- Letux/root/ip-forwarding.sh
+++ Letux/root/ip-forwarding.sh
@@ -15,13 +15,28 @@ Darwin )
 # http://apple.stackexchange.com/questions/228936/using-server-5-0-15-to-share-internet-without-internet-sharing
 # https://superuser.com/questions/931827/sharing-openvpn-on-mac-os-x-yosemite
 
-if ifconfig "utun0" >/dev/null
-then # VPN hides highest priority active network
-	CURRENT="tunnelblick"
-	INTERFACE="utun0"
+INTERFACE=""
 
-else
+# VPN hides highest priority active network
 
+while read LINE
+do
+	NAME=$(echo $LINE | awk -F  "(, )|(: )|[)]" '{print $2}')
+	DEV=$(echo $LINE | awk -F  "(, )|(: )|[)]" '{print $4}')
+	# echo "Current service: $NAME, $DEV, $CURRENT, $INTERFACE"
+	if [ "$DEV" ]
+	then
+		if [ "$NAME" = utun0 ] && ifconfig "$DEV" 2>/dev/null | grep -q 'status: active'
+		then
+			CURRENT="$NAME"
+			INTERFACE="$DEV"
+			break 2	# take first we find
+		fi
+	fi
+done < <(networksetup -listnetworkserviceorder | grep 'Hardware Port')
+
+if [ ! "$INTERFACE" ]
+then
 # locate highest priority active service
 # found at https://apple.stackexchange.com/questions/191879/how-to-find-the-currently-connected-network-service-from-the-command-line
 # and improved...
@@ -42,23 +57,29 @@ else
 		fi
 	done < <(networksetup -listnetworkserviceorder | grep 'Hardware Port')
 
-	if [ -n "$CURRENT" ]
-	then
-		echo Sharing over: $CURRENT $INTERFACE
-	else
-		echo "Could not find current service" >&2
-		exit 1
-	fi
-
 fi
 
+if [ -n "$CURRENT" ]
+then
+	echo Sharing over: $CURRENT $INTERFACE
+else
+	echo "Could not find current service" >&2
+	exit 1
+fi
+
+case "$(uname -r)" in
+21.* | 18.* | 15.* )
+
+echo "(ignore messages about ALTQ)"
 sudo pfctl -F nat
 
-#echo "nat log on $INTERFACE from $DEVICE to any -> ($INTERFACE)
+echo "nat log on $INTERFACE from $DEVICE to any -> ($INTERFACE)"
 #nat log on en0 from $DEVICE to any -> (en0)" | sudo pfctl -N -f - -e
+echo "(ignore messages about -f option flushing rules)"
 echo "nat log on $INTERFACE from $DEVICE to any -> ($INTERFACE)" | sudo pfctl -N -f - -e
 sudo pfctl -a '*' -s nat
 
+echo "sysctl (one of these should succeed)"	# FIXME: we could make this depend on $(uname -r)
 sudo sysctl -w net.inet.ip.forwarding=1
 sudo sysctl -w net.inet.ip.fw.enable=1
 # sudo sysctl -w net.inet6.ip6.forwarding=1
@@ -67,6 +88,8 @@ sudo sysctl -w net.inet.ip.fw.enable=1
 # ifconfig pflog1 create
 # sudo tcpdump -n -e -ttt -i pflog1
 # sudo tcpdump -n -e -ttt -i $INTERFACE
+;;
+esac
 ;;
 
 Linux )
@@ -93,6 +116,6 @@ iptables --append FORWARD --in-interface $IF -j ACCEPT
 ;;
 
 * )
-echo unknown operating system $(uname -s)
+echo unknown operating system $(uname -a)
 ;;
 esac

ba620fa35c5ae1ada9daa32945af3583da7268f1
H. Nikolaus Schaller <hns@goldelico.com>
2025-09-09 22:13:42 +0200

    drm: pvrsgx: 1.14.3699939 fix MIN() problem in mmap for v6.16-rc1
    v6.15.3 introduces a new MIN() macro in linux/minmax.h which
    makes compile trouble in 1.14.3699939 starting with v6.16-rc1
    (and v6.15.3).
    
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- drivers/gpu/drm/pvrsgx/1.14.3699939/eurasia_km/services4/srvkm/env/linux/mmap.h
+++ drivers/gpu/drm/pvrsgx/1.14.3699939/eurasia_km/services4/srvkm/env/linux/mmap.h
@@ -39,6 +39,9 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */ /**************************************************************************/
 
+#undef MIN
+#define MIN(a,b)	(((a) < (b)) ? (a) : (b))
+
 #if !defined(__MMAP_H__)
 #define __MMAP_H__
 
